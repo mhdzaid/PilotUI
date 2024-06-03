d9c0a5e9eb56d240d12863c6ac42de5d5f11ecea
Muhammad Zaid <zaid.mhd@outlook.com>
2024-06-03 10:27:46 +0500

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RobotControlComponent } from './module/robot-control/robot-control.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {RobotService} from "./service/robot.service";
+import {RouterOutlet} from "@angular/router";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RobotControlComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterOutlet
   ],
-  providers: [],
+  providers: [RobotService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
